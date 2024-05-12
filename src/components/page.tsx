fe8bbc05019cc116f4f1f612babdbedb75203c6a
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2024-05-12 22:45:57 +0800

    Implemented example tabs for CLI and Go code examples. Added custom styling for commands documentation page CSS elements. Added code examples for SET command

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
 import CodeBlock from "@theme/CodeBlock";
+
+interface CodeExample {
+  description: string;
+  code: string;
+}
 
 interface Props {
   syntax: string;
@@ -6,13 +12,28 @@ interface Props {
   categories: Array<string>;
   description: string;
   options: Array<string>;
+  examples: {
+    cli: Array<CodeExample>;
+    go: Array<CodeExample>;
+  };
 }
 
-function Page({ syntax, module, categories, description, options }: Props) {
+function Page({
+  syntax,
+  module,
+  categories,
+  description,
+  options,
+  examples,
+}: Props) {
+  const [activeTab, setActiveTab] = useState<"cli" | "go">("cli");
+
   return (
-    <div>
+    <div id="commandPageContainer">
       <div>
-        <b>Sytax:</b>
+        <p>
+          <b>Sytax:</b>
+        </p>
         <CodeBlock>{syntax}</CodeBlock>
       </div>
       <div>
@@ -21,11 +42,13 @@ function Page({ syntax, module, categories, description, options }: Props) {
       </div>
       <div>
         <b>ACL Categories: </b>
-        {categories.map((category, index) => (
-          <span key={`${category}-${index}`}>
-            <span className="">{`@${category.toLowerCase()}`}</span>
-          </span>
-        ))}
+        <div id="aclCategoryContainer">
+          {categories.map((category, index) => (
+            <span key={`${category}-${index}`}>
+              <span className="acl-category">{`@${category.toLowerCase()}`}</span>
+            </span>
+          ))}
+        </div>
       </div>
       <div>
         <b>Description: </b>
@@ -41,6 +64,61 @@ function Page({ syntax, module, categories, description, options }: Props) {
           </ul>
         </div>
       )}
+      <div id="commandExampleContainer">
+        <b>Examples: </b>
+        <div id="commandExampleTabHeader">
+          <span
+            className={`command-example-tab ${
+              activeTab === "cli" ? "active" : ""
+            }`}
+            onClick={() => {
+              setActiveTab("cli");
+            }}
+            role="tab"
+            aria-controls="cliExampleTab"
+            aria-selected={activeTab === "cli"}
+          >
+            CLI
+          </span>
+          <span
+            className={`command-example-tab ${
+              activeTab === "go" ? "active" : ""
+            }`}
+            onClick={() => {
+              setActiveTab("go");
+            }}
+            role="tab"
+            aria-controls="goExampleTab"
+            aria-selected={activeTab === "go"}
+          >
+            Go (Embedded)
+          </span>
+        </div>
+        <div id="commandExampleTabContent">
+          {activeTab === "cli" && (
+            <div id="cliExampleTab">
+              {examples?.cli?.length > 0 &&
+                examples.cli.map((example, index) => (
+                  <div key={`cli-example-${index}`}>
+                    <p>{example.description}</p>
+                    <CodeBlock>{example.code}</CodeBlock>
+                  </div>
+                ))}
+            </div>
+          )}
+          {activeTab === "go" && (
+            <div id="goExampleTab">
+              {examples?.go?.length > 0 &&
+                examples.go.map((example, index) => (
+                  <div key={`go-example-${index}`}>
+                    <p>{example.description}</p>
+                    <CodeBlock language="go">{example.code}</CodeBlock>
+                  </div>
+                ))}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
